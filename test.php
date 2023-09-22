c1caf2997ad618dc543ca9ca2b648a1f2b2c76be
haihek <90749235+haihek@users.noreply.github.com>
2023-09-22 15:13:02 +0200

    Add files via upload

---
+++ test.php
@@ -0,0 +1,36 @@
+﻿<!DOCTYPE html>
+
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <title>Tabela z piwem</title>
+
+    <link href="https://fonts.googleapis.com/css2?family=Croissant+One&display=swap">
+
+    
+
+
+
+</head>
+
+<body style="background-color:cadetblue; font-family: 'Croissant One', cursive; color:red;">
+<b>
+    <table style=" text-shadow: 2px 2px #000000; border-style: dashed; border-color: purple; background-image: url('storm.jpg'); background-size:cover; padding-top: 5px; padding-bottom: 5px; width: 600px; height: 300px;">
+    
+    <?php
+$con = new mysqli("localhost","root","","jakos");
+
+$q = "SELECT * FROM bedzie";
+
+ 
+if($wynik=$con->query($q))
+while($row=$wynik->fetch_array())
+echo "<tr> <th>" . $row["nazwa"] . "</th> <th>" . $row["cena"] . "</th> <th>" . $row["opis"] . "</th></tr>";
+else
+echo $con->errno . " " . $con->error;
+?>
+        
+    </table>
+</b>
+</body>
+</html>
