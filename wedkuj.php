5f390095726bf610e9c06bfe5b670145253579b9
haihek <90749235+haihek@users.noreply.github.com>
2023-09-25 12:55:42 +0200

    Add files via upload

---
+++ wedkuj.php
@@ -0,0 +1,40 @@
+﻿<!DOCTYPE html>
+
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <title>Tabela z piwem</title>
+
+    <link href="https://fonts.googleapis.com/css2?family=Croissant+One&display=swap">
+
+    
+
+
+
+</head>
+
+<body style="background-color:cadetblue; font-family: 'Croissant One', cursive; color:black;">
+<b>
+    </br> </br> </br>
+    
+    <?php
+
+    $pon = 3;
+    $nazwa = "'L%'";
+
+$con = new mysqli("localhost","root","","wedkowanie");
+
+
+$q = "SELECT * FROM ryby WHERE id > " . $pon . " AND nazwa LIKE " . $nazwa;
+
+if($wynik=$con->query($q))
+while($row=$wynik->fetch_array())
+echo $row["id"] . ". " . $row["nazwa"] . " | " . $row["wystepowanie"] . " " . $row["styl_zycia"] . "</br>";
+else
+echo $con->errno . " " . $con->error;
+?>
+        
+    </table>
+</b>
+</body>
+</html>
